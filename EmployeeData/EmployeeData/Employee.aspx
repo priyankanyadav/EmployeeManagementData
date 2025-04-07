71a3d6b3488198010ca85b28f8e701f782fa9a8e
priyanka <priyankanyadav1144@gmail.com>
2025-04-07 11:58:15 -0700

    Add project files.

---
+++ EmployeeData/EmployeeData/Employee.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employee.aspx.cs" Inherits="EmployeeData.EmployeeData.Employee" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title>Employee Management</title>
+    <script type="text/javascript">
+        function clearForm() {
+            document.getElementById('<%= txtEmployeeId.ClientID %>').value = '';
+            document.getElementById('<%= txtName.ClientID %>').value = '';
+            document.getElementById('<%= txtPosition.ClientID %>').value = '';
+            document.getElementById('<%= txtDepartment.ClientID %>').value = '';
+            document.getElementById('<%= btnSave.ClientID %>').value = 'Add Employee';
+        }
+
+        function editEmployee(id, name, position, department) {
+            document.getElementById('<%= txtEmployeeId.ClientID %>').value = id;
+            document.getElementById('<%= txtName.ClientID %>').value = name;
+            document.getElementById('<%= txtPosition.ClientID %>').value = position;
+            document.getElementById('<%= txtDepartment.ClientID %>').value = department;
+            document.getElementById('<%= btnSave.ClientID %>').value = 'Update Employee';
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Employee Management</h2>
+
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+
+            <asp:Panel ID="pnlEmployeeForm" runat="server">
+                <asp:Label ID="lblEmployeeId" runat="server" Text="Employee ID" Visible="false"></asp:Label>
+                <asp:TextBox ID="txtEmployeeId" runat="server" Visible="false"></asp:TextBox><br />
+
+                <asp:Label ID="lblName" runat="server" Text="Name:"></asp:Label>
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br />
+
+                <asp:Label ID="lblPosition" runat="server" Text="Position:"></asp:Label>
+                <asp:TextBox ID="txtPosition" runat="server"></asp:TextBox><br />
+
+                <asp:Label ID="lblDepartment" runat="server" Text="Department:"></asp:Label>
+                <asp:TextBox ID="txtDepartment" runat="server"></asp:TextBox><br />
+
+                <asp:Button ID="btnSave" runat="server" Text="Add Employee" OnClick="btnSave_Click" /><br />
+            </asp:Panel>
+
+            <h2>Employee List</h2>
+            <asp:GridView ID="gvEmployees" runat="server" AutoGenerateColumns="False" OnRowCommand="gvEmployees_RowCommand">
+                <Columns>
+                    <asp:BoundField DataField="EmployeeId" HeaderText="ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Position" HeaderText="Position" />
+                    <asp:BoundField DataField="Department" HeaderText="Department" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:Button ID="btnEdit" runat="server" Text="Edit" CommandName="EditEmployee" CommandArgument='<%# Eval("EmployeeId") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EmployeeData/EmployeeData/Employee.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace EmployeeData.EmployeeData
+{
+    public partial class Employee : System.Web.UI.Page
+    {
+        private string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["EmployeeDBConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindEmployeeGrid();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEmployeeId.Text))
+            {
+                // Add new employee
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Employees (Name, Position, Department) VALUES (@Name, @Position, @Department)";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Position", txtPosition.Text);
+                    cmd.Parameters.AddWithValue("@Department", txtDepartment.Text);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+                lblMessage.Text = "Employee added successfully!";
+            }
+            else
+            {
+                // Update existing employee
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Employees SET Name = @Name, Position = @Position, Department = @Department WHERE EmployeeId = @EmployeeId";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@EmployeeId", int.Parse(txtEmployeeId.Text));
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Position", txtPosition.Text);
+                    cmd.Parameters.AddWithValue("@Department", txtDepartment.Text);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+                lblMessage.Text = "Employee updated successfully!";
+            }
+
+            clearForm();
+            BindEmployeeGrid();
+        }
+
+        private void BindEmployeeGrid()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Employees";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                gvEmployees.DataSource = reader;
+                gvEmployees.DataBind();
+
+                con.Close();
+            }
+        }
+
+        protected void gvEmployees_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditEmployee")
+            {
+                int employeeId = Convert.ToInt32(e.CommandArgument);
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Employees WHERE EmployeeId = @EmployeeId";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@EmployeeId", employeeId);
+
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        txtEmployeeId.Text = reader["EmployeeId"].ToString();
+                        txtName.Text = reader["Name"].ToString();
+                        txtPosition.Text = reader["Position"].ToString();
+                        txtDepartment.Text = reader["Department"].ToString();
+
+                        btnSave.Text = "Update Employee";
+                    }
+                    con.Close();
+                }
+            }
+        }
+
+        private void clearForm()
+        {
+            txtEmployeeId.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtPosition.Text = string.Empty;
+            txtDepartment.Text = string.Empty;
+
+            btnSave.Text = "Add Employee";
+        }
+    }
+}
